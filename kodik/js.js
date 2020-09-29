9351c50da84301c9d65f7d365225fd63aadbaecf
Springer <springer@spstrutnov.cz>
2020-09-29 13:16:13 +0200

    Pauza

---
+++ kodik/js.js
@@ -0,0 +1,33 @@
+function zapisCisla() 
+{
+    if(0(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 0;
+    if(1(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 1;
+    if(2(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 2;
+    if(3(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 3;
+    if(4(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 4;
+    if(5(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 5;
+    if(6(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 6;
+    if(7(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 7;
+    if(8(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 8;
+    if(9(true))
+        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 9;
+
+}
+
+
+function pocitani()
+{
+
+
+
+
+}

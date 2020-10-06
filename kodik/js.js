30a9ee367f8b46993c22191ee064ba52011e4aea
Springer <springer@spstrutnov.cz>
2020-10-06 12:38:07 +0200

    Psani cislic

--- kodik/js.js
+++ kodik/js.js
@@ -1,33 +1,77 @@
-function zapisCisla() 
-{
-    if(0(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 0;
-    if(1(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 1;
-    if(2(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 2;
-    if(3(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 3;
-    if(4(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 4;
-    if(5(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 5;
-    if(6(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 6;
-    if(7(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 7;
-    if(8(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 8;
-    if(9(true))
-        document.getElementById("hodnota").value = document.getElementById("hodnota").value + 9;
+var hodnota = "";
 
+function _0()
+{
+    hodnota += 0;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _1() 
+{
+    hodnota += 1;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _2()
+ {
+    hodnota += 2;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _3()
+ {
+    hodnota += 3;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _4()
+ {
+    hodnota += 4;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _5()
+ {
+    hodnota += 5;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _6()
+ {
+    hodnota += 6;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _7()
+ {
+    hodnota += 7;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _8()
+ {
+    hodnota += 8;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
+}
+function _9()
+ {
+    hodnota += 9;   
+
+    document.getElementById("hodnota").innerHTML = hodnota;
+    console.log(hodnota);
 }
 
-
-function pocitani()
+function plus()
 {
-
-
-
-
+    
 }

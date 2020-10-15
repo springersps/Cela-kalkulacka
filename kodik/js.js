00cdf82a508f66c66bfc0214554cca85aad5e413
Springer <springer@spstrutnov.cz>
2020-10-15 14:23:09 +0200

    Uprava, pridani odcitani, nasobeni, delene, druhe mocniny, odmocniny

--- kodik/js.js
+++ kodik/js.js
@@ -1,5 +1,6 @@
 var hodnota = "";
 var vysledek = "";
+var znak = "";
 
 function _0()
 {
@@ -74,16 +75,87 @@ function _9()
 
 function plus()
 {
+    znak = "plus";
     vysledek = hodnota;
 
-    parseInt(hodnota);
-    parseInt(vysledek);
-
-    vysledek += hodnota;
     hodnota = "";
+}
+
+function minus()
+{
+    znak = "minus"
+    if(vysledek == "")
+    {
+        vysledek = hodnota
+    }
+
+    hodnota = "";
+}
+
+function nasobeni()
+{
+    znak = "nasobeni";
+    if(vysledek == ""){
+        vysledek = hodnota;
+    }
+
+    hodnota = "";
+}
+
+function deleno()
+{
+    znak = "deleno";
+    if(vysledek == ""){
+        vysledek = hodnota;
+    }
+
+    hodnota = "";
+}
+
+function mocnina2()
+{
+    znak = "mocnina2";
+    if(vysledek == ""){
+    vysledek = hodnota;
+    }
+}
+
+function druhaodmocnina()
+{
+    znak = "druhaodmocnina";
+    if(vysledek == ""){
+        vysledek = hodnota;
+    }
 }
 
 function pocitani()
 {
+    if(znak == "plus")
+    {
+        parseInt(hodnota);
+        parseInt(vysledek);
+
+        vysledek += hodnota;
+    }
+
+    if(znak == "minus"){
+        vysledek -= hodnota;
+    }
+
+    if(znak == "nasobeni"){
+        vysledek *= hodnota;
+    }
+
+    if(znak == "deleno"){
+        vysledek /= hodnota;
+    }
+
+    if(znak == "mocnina2"){
+        vysledek = vysledek * vysledek;
+    }
+    if(znak == "druhaodmocnina"){
+        vysledek = Math.sqrt(vysledek);
+    }
+
     console.log(vysledek);
 }
